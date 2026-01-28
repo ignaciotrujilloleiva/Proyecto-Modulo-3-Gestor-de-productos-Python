2e167142b82b13b9058eec065852a94e1e895bfb
ignaciotrujilloleiva <ignacio.trujilloleiva@gmail.com>
2026-01-28 18:22:25 -0300

    import mostrar menu
    + Creación Print de prueba
    + Creación de menu interactivo en menu.py
    + Importación y prueba de función mostrar_menu desde menu.py

---
+++ main.py
@@ -0,0 +1,17 @@
+# Sistema de Gestión de Productos
+# Autor: Ignacio Trujillo
+# Descripción: Programa por consola para gestionar productos usando Python
+
+
+#importaciones
+from modulos.menu import mostrar_menu
+
+#Impresion de prueba 
+print ("Hola \nBienvenido")
+
+#Funcion principal
+def main():
+    print("Bienvenido al sistema de gestion de productos")
+    mostrar_menu()
+
+main()
---
+++ modulos/menu.py
@@ -0,0 +1,28 @@
+# Menu interactivo
+
+def mostrar_menu():
+    while True: 
+        print("--- Menú Principal ---")
+        print("1. Agregar producto")
+        print("2. Listar productos")
+        print("3. buscar productos")
+        print("4. Eliminar productos")
+        print("5. Salir")
+
+
+        opcion = input("Seleccione una opción: ")
+
+
+        if opcion == "1":
+            print("Opción agregar producto ")
+        elif opcion == "2":
+            print("Opción listar productos ")
+        elif opcion =="3":
+            print("Opción buscar producto ")
+        elif opcion == "4":
+            print("Opcion eliminar producto ")
+        elif opcion == "5":
+            print("Saliendo del sistema ...")
+            break
+        else:
+            print("Opción inválida, intente nuevamente")
