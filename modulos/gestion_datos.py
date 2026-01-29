39b66ca3590f48b6196d37ec256cb318a7d23b72
ignaciotrujilloleiva <ignacio.trujilloleiva@gmail.com>
2026-01-28 23:55:17 -0300

    Update gestion_datos.py
    +Se incorpora la variable ids_registrados =set() para evitar los duplicados de los productos mediante los ID´s

--- modulos/gestion_datos.py
+++ modulos/gestion_datos.py
@@ -1,12 +1,26 @@
 # Lección 4: Control de flujo y estructuras
+# Lección 5: Estructuras de datos
+
 
 
 #lista de productos, que tendra los productos en forma de diccionario
 productos = []
 
+#Usaremos set para evitar los duplicados en los ID´s
+ids_registrados = set()
+
+
 #función para agregar producto a la lista
 def agregar_producto(producto):
+    #Se agrega verificación de ID´s
+    if producto["id"] in ids_registrados:
+        print("Error: ID ya se encuentra en el sistema")
+        return False
+
+    #Si id no se encuentra en los ids_registrados, se agrega el producto
     productos.append(producto)
+    ids_registrados.add(producto["id"])
+    return True
 
 #función para listar los productos con sus atributos
 def listar_productos():
@@ -23,3 +37,4 @@ def listar_productos():
             f"Stock: {producto['stock']} |"
             f"Categoría: {producto['categoria']}" 
         )
+
