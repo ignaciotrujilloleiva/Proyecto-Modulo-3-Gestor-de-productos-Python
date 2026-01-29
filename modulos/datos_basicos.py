77caf7300e9fd44053f18ae74f3bec316e400525
ignaciotrujilloleiva <ignacio.trujilloleiva@gmail.com>
2026-01-28 21:21:43 -0300

    Update datos_basicos.py
    Lección 2
    +Creación de función para cargar productos.
    +Se crea tupla para las categorías.
    +validaciones de tipo para datos ingresados por el usuario.
    +ciclo for para enumeración de categorías disponibles.
    +ciclo while para verificar que el usuario seleccione una categoría valida
    + definición de atributos del producto

---
+++ modulos/datos_basicos.py
@@ -0,0 +1,42 @@
+# Lección 2: Tipos de datos y sentencias básicas
+
+#Diccionario con productos
+
+
+#Tupla para las categorias
+categorias = ("cat1","cat2","cat3","cat4")
+
+
+#Declaración de variables para recibir y almacenar datos
+def cargar_producto(id_producto):
+    #solicita al usuario el ingreso de datos del producto
+    nombre = input("Ingrese nombre del producto: ")
+    precio = float(input("Ingrese precio del producto: "))
+    stock = int(input("Ingrese stock del producto: "))
+
+    print("Categorias disponibles: ")
+    #imprime las categorias disponibles
+    for item,categoria in enumerate(categorias, start=1):
+        print({f"{item}. {categoria}"})
+
+    #Validación de elección de categoria
+    while True:
+        opcion = int(input("Seleccione categoría (número): "))
+        if opcion >=1 and opcion <= len(categorias):
+            categoria = categorias[opcion - 1]
+            break
+        else:
+            print("Categoría inválida")
+    
+
+    producto = {
+        "ID": id_producto,
+        "nombre": nombre,
+        "precio": precio,
+        "stock": stock,
+        "categoria": categoria,
+        
+    }
+
+    return producto
+cargar_producto(1)
